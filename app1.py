b93e38015ac65b29c73a61c6841ba4c73a35a0ce
cecurado <cecurado@hotmail.com>
2025-07-01 22:26:47 -0400

    Update app1.py

--- app1.py
+++ app1.py
@@ -1,12 +1,14 @@
 import streamlit as st
 
-st.title("Here is my AI 'Agent' ")
-st.write("I knew you could read my codes and understand my broken english before 2026")
+st.title("Here is my AI 'Agent' LOL  ")
+st.write("I knew you could read my codes and understand my broken english before 2026 😌 ")
 
 st.markdown("""
-### 🌸 I hope you are having an amazing day/night 🌸
+###  I hope you are having an amazing day/night  
 
 Thank you for letting me get to know you. I hope this makes you smile today! 😊
 """)
 
-st.balloons()
+if st.button("Click me!"):
+    st.write(" I hope this makes you smile today! 😊")
+    st.balloons()
