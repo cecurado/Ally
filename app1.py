1fb026c6daa72dcc815b1f0dfaff6271b3fce0ad
cecurado <cecurado@hotmail.com>
2025-07-01 22:17:57 -0400

    Create app1.py
    first app atempt

---
+++ app1.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.title("Here is my AI 'Agent' ")
+st.write("I knew you could read my codes and understand my broken english before 2026")
+
+st.markdown("""
+### 🌸 I hope you are having an amazing day/night 🌸
+
+Thank you for letting me get to know you. I hope this makes you smile today! 😊
+""")
+
+st.balloons()
